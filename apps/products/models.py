0e0d132b976020efb03af3938c5d825f96612440
Dinesh K <d860848@gmail.com>
2026-06-19 22:31:05 +0530

    Clean repository and remove generated files

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,16 +1,68 @@
 from django.db import models
+from apps.categories.models import Category
 
-# Create your models here.
 class Product(models.Model):
-    category = models.ForeignKey('categories.category', 
-    on_delete=models.CASCADE, related_name='products', null=True, blank=True)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="products"
+    )
+
     name = models.CharField(max_length=255)
+
+    slug = models.SlugField(
+        unique=True,
+        blank=True
+    )
+
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    discount_price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        null=True,
+        blank=True
+    )
+
     stock = models.PositiveIntegerField(default=0)
-    image = models.ImageField(upload_to='products/', blank=True, null=True)
+
+    sku = models.CharField(
+        max_length=100,
+        unique=True
+    )
+
+    image = models.ImageField(
+        upload_to="products/",
+        null=True,
+        blank=True
+    )
+
+    fabric = models.CharField(
+        max_length=100,
+        blank=True
+    )
+
+    color = models.CharField(
+        max_length=100,
+        blank=True
+    )
+
+    weight = models.CharField(
+        max_length=50,
+        blank=True
+    )
+
+    is_featured = models.BooleanField(default=False)
+
     is_active = models.BooleanField(default=True)
+
     created_at = models.DateTimeField(auto_now_add=True)
+
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
--- apps/products/views.py
+++ apps/products/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render,
+get_object_or_404()
+from .models import Product
+def product_details(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    return render(request, 'products/product_details.html', {'product': product})
